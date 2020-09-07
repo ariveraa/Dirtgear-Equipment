38522e25f459d50e6a4316680faeef153a291a13
Adrian Rivera <riveracavana@gmail.com>
2020-09-06 21:05:28 -0600

    fixed server and started styling

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import '/nav.scss'; 
 import {withRouter} from 'react-router-dom'; 
 
 const Nav = (props) => { 
 
     return(
-        <div> 
+        <div className = 'nav-container'> 
             {/* <img src = './dirt_gear_logo.png' /> */}
-            <div onClick = {() => props.history.push('/')}>Home</div>
-            <div onClick = {() => props.history.push('/login')}>Login</div>
-            <div onClick = {() => props.history.push('/registration')}>Register</div>
-            <div onClick = {() => props.history.push('/profile') }>Profile</div>
-            <div onClick = {() => props.history.push('/equipment')}>Equipment</div> 
-            <div onClick = {() => props.history.push('/myequipment')}>My Equipment</div> 
+            <div className = 'nav-slector' onClick = {() => props.history.push('/')}>Home</div>
+            <div className = 'nav-slector' onClick = {() => props.history.push('/login')}>Login</div>
+            <div className = 'nav-slector'  onClick = {() => props.history.push('/registration')}>Register</div>
+            <div className = 'nav-slector'  onClick = {() => props.history.push('/profile') }>Profile</div>
+            <div className = 'nav-slector'  onClick = {() => props.history.push('/equipment')}>Equipment</div> 
+            <div className = 'nav-slector'  onClick = {() => props.history.push('/myequipment')}>My Equipment</div> 
         </div>
     )
 }
